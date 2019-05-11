26cf94822d62efb5ff78357b7b6b0ba43db1dcfc
Thiago Costa <thiagogcv@gmail.com>
2019-05-11 18:34:59 -0300

    Adicionando arquivo js

---
+++ assets/js/todo.js
@@ -0,0 +1,19 @@
+let data = [{
+    id: 1,
+    title: "Estudar HTML"
+}, {
+    id: 2,
+    title: "Estudar CSS"
+}, {
+    id: 3,
+    title: "Estudar JavaScript"
+}, {
+    id: 4,
+    title: "Estudar NodeJS"
+
+}, {
+    id: 5,
+    title: "Estudar React"
+}];
+
+console.log(data);
