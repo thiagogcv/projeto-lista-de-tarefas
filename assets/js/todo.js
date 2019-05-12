f25efb6f48c7105a6c00e5574a5c8a8ae81f7311
Thiago Costa <thiagogcv@gmail.com>
2019-05-12 02:10:12 -0300

    commit

--- assets/js/todo.js
+++ assets/js/todo.js
@@ -1,43 +1,70 @@
-let data = [{
-    id: 1,
-    title: "Estudar HTML"
-}, {
-    id: 2,
-    title: "Estudar CSS"
-}, {
-    id: 3,
-    title: "Estudar JavaScript"
-}, {
-    id: 4,
-    title: "Estudar NodeJS"
+let data = [];
 
-}, {
-    id: 5,
-    title: "Estudar React"
-}];
+function renderTodo() {
 
-data.forEach(task => {
-    let li = document.createElement('li');
+    document.querySelector('.todo').innerHTML = '';
 
-    //Template da lista de tarefas    
-    li.innerHTML = `
-    <input type="checkbox" id="task-${task.id}">
-    <label for="${task.id}">${task.title}</label>
-    `;
+    data.forEach(task => {
 
-    //Adicionado evento de ckecked na lista de tarefa completada
-    li.querySelector('input').addEventListener('change', e =>{
+        let li = document.createElement('li');
 
-        if(e.target.checked){
-            li.classList.add('complete')
+        li.innerHTML = `
+            <input type="checkbox" id="task-${task.id}">
+            <label for="task-${task.id}">${task.title}</label>
+            <button type="button">x</button> 
+        `;
 
-        }else{
-            li.classList.remove('complete')
-        }
+        li.querySelector('input').addEventListener("change", e => {
 
-         
-    })
+            if (e.target.checked) {
+                li.classList.add('complete');
+            } else {
+                li.classList.remove('complete');
+            }
 
-    document.querySelector('.todo').append(li);
+        });
+
+        li.querySelector('button').addEventListener('click', e => {
+
+            let button = e.target;
+            let li = button.parentNode;
+            let input = li.querySelector('input');
+            let id = input.id;
+            let idArray = id.split('-');
+            let todoId = idArray[1];
+            let title = li.querySelector('label').innerText;
+
+            if (confirm(`Deseja realmente excluir a tarefa ${title}?`)) {
+
+                data = data.filter(task => task.id !== parseInt(todoId));
+
+                renderTodo();
+
+            }            
+
+        });
+
+        document.querySelector('.todo').append(li);
+
+    });
+
+}
+
+document.querySelector('#new-task').addEventListener('keyup', e => {
+
+    if (e.key === 'Enter') {
+
+        data.push({
+            id: data.length+1,
+            title: e.target.value
+        });
+
+        e.target.value = "";
+
+        renderTodo();
+
+    }
 
 });
+
+renderTodo();
